1288ae2d503f38edad4e427e94f6c32fd2105855
Вишняков Владимир <vladimir.vishnyakov@gazprombank.ru>
2024-10-22 09:10:04 +0300

    Рефакторинг

--- app/src/main/java/com/example/aheena/di/modules/UtilsModule.kt
+++ app/src/main/java/com/example/aheena/di/modules/UtilsModule.kt
@@ -2,8 +2,8 @@ package com.example.aheena.di.modules
 
 import androidx.lifecycle.ViewModelProvider
 import com.example.core.utils.StringProvider
-import com.example.core.utils.StringProviderImpl
 import com.example.core_impl.utils.AppViewModelFactory
+import com.example.core_impl.utils.StringProviderImpl
 import dagger.Binds
 import dagger.Module
 
