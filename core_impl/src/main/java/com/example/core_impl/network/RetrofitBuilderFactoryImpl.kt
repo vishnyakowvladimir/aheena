ff0673860481a7615537ee675a3c184de5a890aa
Вишняков Владимир <vladimir.vishnyakov@gazprombank.ru>
2024-11-12 10:07:58 +0300

    Добавление ScalarsConverterFactory
    Закомментировал, потому что пока не подтягивается

--- core_impl/src/main/java/com/example/core_impl/network/RetrofitBuilderFactoryImpl.kt
+++ core_impl/src/main/java/com/example/core_impl/network/RetrofitBuilderFactoryImpl.kt
@@ -5,12 +5,14 @@ import com.example.core.network.RetrofitBuilderFactory.Companion.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
+//import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class RetrofitBuilderFactoryImpl @Inject constructor() : RetrofitBuilderFactory {
 
     override fun createBuilder(): Retrofit.Builder {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+//            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
     }
 }
