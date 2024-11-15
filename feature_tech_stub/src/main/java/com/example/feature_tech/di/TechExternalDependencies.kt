67d16f76c69ac32bbaabc9e8cbf29215a9d8cc52
Вишняков Владимир <vladimir.vishnyakov@gazprombank.ru>
2024-11-15 07:25:01 +0300

    Рефакторинг

--- feature_tech_stub/src/main/java/com/example/feature_tech/di/TechExternalDependencies.kt
+++ feature_tech_stub/src/main/java/com/example/feature_tech/di/TechExternalDependencies.kt
@@ -1,15 +1,5 @@
 package com.example.feature_tech.di
 
-import android.content.Context
-import com.example.core_api.di.qualifier.MainRouter
-import com.example.core_api.holder.ActivityHolder
-import com.example.core_api.navigation.router.NavRouter
-import com.example.core_api.utils.string_provider.StringProvider
 import javax.inject.Inject
 
-class TechExternalDependencies @Inject constructor(
-    val applicationContext: Context,
-    val activityHolder: ActivityHolder,
-    @get:MainRouter @MainRouter val mainRouter: NavRouter,
-    val stringProvider: StringProvider,
-)
+class TechExternalDependencies @Inject constructor()
