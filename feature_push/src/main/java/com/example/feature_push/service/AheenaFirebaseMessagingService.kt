2bf9120003a8e0ad67c5f90f35e1d6691e50b9d1
v.vishniakov <v.vishniakov@MACWS-060.local>
2025-08-25 08:10:40 +0300

    Добавление Firebase Messaging

--- feature_push/src/main/java/com/example/feature_push/service/AheenaFirebaseMessagingService.kt
+++ feature_push/src/main/java/com/example/feature_push/service/AheenaFirebaseMessagingService.kt
@@ -1,9 +1,23 @@
 package com.example.feature_push.service
 
+import android.Manifest
+import android.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresPermission
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.core_api.log.LOG_TAG
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import androidx.core.net.toUri
+
+private const val PUSH_NOTIFICATION_CHANNEL_ID = "PUSH_NOTIFICATION_CHANNEL_ID"
 
 class AheenaFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -13,8 +27,56 @@ class AheenaFirebaseMessagingService : FirebaseMessagingService() {
         /* Отправка токена на бэк */
     }
 
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Log.d(LOG_TAG, "message: $message")
+        val title = message.data["title"] ?: ""
+        val description = message.data["description"] ?: ""
+        val url = message.data["url"] ?: ""
+        showNotification(
+            title = title,
+            description = description,
+            url = url,
+        )
+    }
+
+    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+    private fun showNotification(
+        title: String,
+        description: String,
+        url: String,
+    ) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                PUSH_NOTIFICATION_CHANNEL_ID,
+                "Push Notifications",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+
+        val intent = Intent("android.intent.action.MAIN").apply {
+            data = url.toUri()
+            setPackage(packageName) // обязательно, чтобы не ловили чужие приложения
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val notification = NotificationCompat.Builder(this, PUSH_NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_dialog_info)
+            .setContentTitle(title)
+            .setContentText(description)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        NotificationManagerCompat.from(this).notify(System.currentTimeMillis().toInt(), notification)
     }
 }
